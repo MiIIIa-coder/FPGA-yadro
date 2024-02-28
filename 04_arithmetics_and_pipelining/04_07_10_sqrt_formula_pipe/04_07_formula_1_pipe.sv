4e01339e877c8589ba1ed824ce24b0d9828b2141
Matvienko Mikhail <matvienko.mk@phystech.edu>
2024-02-29 00:04:11 +0300

    task 7 bad

--- 04_arithmetics_and_pipelining/04_07_10_sqrt_formula_pipe/04_07_formula_1_pipe.sv
+++ 04_arithmetics_and_pipelining/04_07_10_sqrt_formula_pipe/04_07_formula_1_pipe.sv
@@ -12,8 +12,8 @@ module formula_1_pipe
     input  [31:0] b,
     input  [31:0] c,
 
-    output        res_vld,
-    output [31:0] res
+    output logic    res_vld,
+    output logic [31:0] res
 );
     // Task:
     //
@@ -41,5 +41,75 @@ module formula_1_pipe
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm
 
+    //'include "isqrt.sv";
+
+    logic [31:0] sqrt_a;
+    logic [31:0] sqrt_b;
+    logic [31:0] sqrt_c;
+    logic [31:0] sum;
+
+    logic vld_a;
+    logic vld_b;
+    logic vld_c;
+
+    logic isqrt_vld;
+    logic sum_vld;
+
+    isqrt   isqrt1
+    (
+        .clk (clk),
+        .rst(rst), 
+        .x_vld(arg_vld), 
+        .x(a),
+
+        .y_vld(vld_a), 
+        .y(sqrt_a)
+    );
+    
+    isqrt   isqrt2
+    (
+        .clk (clk),
+        .rst(rst), 
+        .x_vld(arg_vld), 
+        .x(b),
+
+        .y_vld(vld_b), 
+        .y(sqrt_b)
+    );
+
+    isqrt   isqrt3
+    (
+        .clk (clk),
+        .rst(rst), 
+        .x_vld(arg_vld), 
+        .x(c),
+
+        .y_vld(vld_c), 
+        .y(sqrt_c)
+    );
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+            res_vld <= '0;
+        else
+            res_vld <= sum_vld; //valid from adder
+
+    always_ff @ (posedge clk or posedge rst) //??? нужен ли?
+        if (rst)
+            sum_vld <= '0;
+        else
+            sum_vld <= isqrt_vld;
+        
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+            isqrt_vld <= '0;
+        else
+            isqrt_vld <= (vld_a & vld_b & vld_c);
+    
+    assign sum = sqrt_a + sqrt_b + sqrt_c;
+
+    always_ff @ (posedge clk)
+        if (isqrt_vld)
+            res <= sum;
 
 endmodule
