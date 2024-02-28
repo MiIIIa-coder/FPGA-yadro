931ccb70b1c542d66db3eaaafd698bb2a92ce4ad
Matvienko Mikhail <matvienko.mk@phystech.edu>
2024-02-28 19:19:50 +0300

    6 task

--- 04_arithmetics_and_pipelining/04_06_arithmetic_shift_or_signed_divide_by_power_of_2.sv
+++ 04_arithmetics_and_pipelining/04_06_arithmetic_shift_or_signed_divide_by_power_of_2.sv
@@ -17,7 +17,7 @@ endmodule
 
 module arithmetic_right_shift_of_N_by_S_using_concatenation
 # (parameter N = 8, S = 3)
-(input  [N - 1:0] a, output [N - 1:0] res);
+(input  [N - 1:0] a, output logic [N - 1:0] res);
 
   // Task:
   //
@@ -25,6 +25,8 @@ module arithmetic_right_shift_of_N_by_S_using_concatenation
   // but without using ">>>" operation. You are allowed to use only
   // concatenations ({a, b}), bit repetitions ({ a { b }}), bit slices
   // and constant expressions.
+
+  assign res = {{S{a[N - 1]}}, {a[N - 1:S]}};
 
 
 endmodule
@@ -40,6 +42,14 @@ module arithmetic_right_shift_of_N_by_S_using_for_inside_always
   // You are allowed to use only "always_comb" with a "for" loop
   // that iterates through the individual bits of the input.
 
+  always_comb 
+    for (int i = 0; i < N; i++)
+      // if (i > N - 1 - S)
+      //   res[i] = a[N - 1];
+      // else
+      //   res[i] = a[i + S];
+      res[i] = i > N - 1 - S ? a[N - 1] : a[i + S];
+
 
 endmodule
 
@@ -48,8 +58,19 @@ module arithmetic_right_shift_of_N_by_S_using_for_inside_generate
 (input  [N - 1:0] a, output [N - 1:0] res);
 
   // Task:
+  //
   // Implement a module that arithmetically shifts input exactly
   // by `S` bits to the right using "generate" and "for"
+
+  genvar i;
+
+  generate
+    for (i = 0; i < N; i++)
+      if (i > N - 1 - S)
+        assign res[i] = a[N - 1];
+      else
+        assign res[i] = a[i + S];
+  endgenerate
 
 
 endmodule
