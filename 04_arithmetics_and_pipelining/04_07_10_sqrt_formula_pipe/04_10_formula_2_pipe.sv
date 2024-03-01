fe2e54eefeadbac296314f3330d9af8e863a45cc
Matvienko Mikhail <matvienko.mk@phystech.edu>
2024-03-01 22:35:35 +0300

    10 task

--- 04_arithmetics_and_pipelining/04_07_10_sqrt_formula_pipe/04_10_formula_2_pipe.sv
+++ 04_arithmetics_and_pipelining/04_07_10_sqrt_formula_pipe/04_10_formula_2_pipe.sv
@@ -12,8 +12,8 @@ module formula_2_pipe
     input  [31:0] b,
     input  [31:0] c,
 
-    output        res_vld,
-    output [31:0] res
+    output logic        res_vld,
+    output logic [31:0] res
 );
     // Task:
     //
@@ -40,6 +40,125 @@ module formula_2_pipe
     // in the article by Yuri Panchul published in
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm
+
+    logic [15:0] sqrt_a;  //sqrt(a)
+    logic [15:0] sqrt_b;  //sqrt(b + sqrt(a))
+    logic [15:0] sqrt_c;  //sqrt(c + sqrt(b + sqrt(a)))
+    logic [31:0] sum1;    //b + sqrt(a)
+    logic [31:0] sum2;    //c + sqrt(b + sqrt(a))
+
+    logic [31:0] out_reg_s1;  //out of shift register 1
+    logic [31:0] out_reg_s2;  //out of shift register 1
+
+    logic vld_sqrt_a;
+    logic vld_sqrt_b;
+    logic vld_sqrt_c;
+    logic vld_sqrt_reg1;  //valid register for isqrt1
+    logic vld_sqrt_reg2;  //valid register for isqrt2
+    logic vld_sh_r_reg1;  //valid register for shift register1
+    logic vld_sh_r_reg2;  //valid register for shift register2
+
+    logic vld_reg_s1;     //valid out_data of shift register1
+    logic vld_reg_s2;     //valid out_data of shift register2
+
+    //logic res_reg;  //register for res
+
+    //-----------------------------------------------
+    // ISQRT instances
+
+    isqrt   isqrt1
+    (
+        .clk(clk),
+        .rst(rst), 
+        .x_vld(arg_vld), 
+        .x(a),
+
+        .y_vld(vld_sqrt_a), 
+        .y(sqrt_a)
+    );
+    
+    isqrt   isqrt2
+    (
+        .clk(clk),
+        .rst(rst), 
+        .x_vld(vld_sqrt_reg1 & vld_sh_r_reg1), 
+        .x(sum1),
+
+        .y_vld(vld_sqrt_b), 
+        .y(sqrt_b)
+    );
+
+    isqrt   isqrt3
+    (
+        .clk(clk),
+        .rst(rst), 
+        .x_vld(vld_sqrt_reg2 & vld_sh_r_reg2), 
+        .x(sum2),
+
+        .y_vld(vld_sqrt_c), 
+        .y(sqrt_c)
+    );
+
+    //-----------------------------------------------
+    // shift register instances
+
+    shift_register_with_valid #(32, 16) s_r_1
+    (
+        .clk(clk),
+        .rst(rst),
+        .in_vld(arg_vld),
+        .in_data(b),
+
+        .out_vld(vld_reg_s1),
+        .out_data(out_reg_s1)
+    );
+
+    shift_register_with_valid #(32, 33) s_r_2
+    (
+        .clk(clk),
+        .rst(rst),
+        .in_vld(arg_vld),
+        .in_data(c),
+
+        .out_vld(vld_reg_s2),
+        .out_data(out_reg_s2)
+    );
+
+    //-----------------------------------------------
+    // LOGIC
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+            res_vld <= 'b0;
+        else
+            res_vld <= vld_sqrt_c;
+
+    always_ff @ (posedge clk)
+    begin
+        //SUM1: b + sqrt(a)
+        if (vld_sqrt_a & vld_reg_s1)
+            sum1 <= sqrt_a + out_reg_s1;
+        
+        //SUM2: c + sqrt(b + sqrt(a))
+        if (vld_sqrt_b & vld_reg_s2)
+            sum2 <= sqrt_b + out_reg_s2;
+
+        //RES: sqrt(c + sqrt(b + sqrt(a)))
+        if (vld_sqrt_c)
+            res <= sqrt_c;
+    end
+
+    //-----------------------------------------------
+    // valid registers
+    
+    always_ff @ (posedge clk)
+    begin
+        vld_sqrt_reg1 <= vld_sqrt_a;
+        vld_sqrt_reg2 <= vld_sqrt_b;
+
+        vld_sh_r_reg1 <= vld_reg_s1;
+        vld_sh_r_reg2 <= vld_reg_s2;
+    end
 
 
 endmodule
