e826a68226baa4347c844b3e1b7d035a43360291
Matvienko Mikhail <matvienko.mk@phystech.edu>
2024-03-29 16:31:57 +0300

    task 6

--- 05_fifo_and_single_cycle_cpu/05_06_sqrt_formula_pipe/05_06_formula_2_pipe_using_fifos.sv
+++ 05_fifo_and_single_cycle_cpu/05_06_sqrt_formula_pipe/05_06_formula_2_pipe_using_fifos.sv
@@ -12,8 +12,8 @@ module formula_2_pipe_using_fifos
     input  [31:0] b,
     input  [31:0] c,
 
-    output        res_vld,
-    output [31:0] res
+    output logic        res_vld,
+    output logic [31:0] res
 );
     // Task:
     //
@@ -40,6 +40,124 @@ module formula_2_pipe_using_fifos
     // in the article by Yuri Panchul published in
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm
+
+    logic [15:0] sqrt_a;  //sqrt(a + sqrt(b + sqrt(c)))
+    logic [15:0] sqrt_b;  //sqrt(b + sqrt(c))
+    logic [15:0] sqrt_c;  //sqrt(c)
+    logic vld_sqrt_c;
+    logic vld_sqrt_b;
+    logic vld_sqrt_a;
+    logic [31:0] sum1;    //b + sqrt(c)
+    logic [31:0] sum2;    //a + sqrt(b + sqrt(c))
+
+    logic vld_sqrt_reg_1;  //valid register for isqrt1
+    logic vld_sqrt_reg_2;  //valid register for isqrt2
+
+    logic [31:0] out_fifo_1; //data from FIFO1
+    logic [31:0] out_fifo_2; //data from FIFO2
+
+    logic empty_fifo_1;
+    logic empty_fifo_2;
+
+    logic full_fifo_1;
+    logic full_fifo_2;
+
+    //-------------------------------------------------
+    // sqrt instances
+    //
+
+    isqrt   isqrt1
+    (
+        .clk(clk),
+        .rst(rst),
+        .x_vld(arg_vld), 
+        .x(c),
+
+        .y_vld(vld_sqrt_c), 
+        .y(sqrt_c)
+    );
+    
+    isqrt   isqrt2
+    (
+        .clk(clk),
+        .rst(rst), 
+        .x_vld(vld_sqrt_reg_1), 
+        .x(sum1),
+
+        .y_vld(vld_sqrt_b), 
+        .y(sqrt_b)
+    );
+
+    isqrt   isqrt3
+    (
+        .clk(clk),
+        .rst(rst), 
+        .x_vld(vld_sqrt_reg_2), 
+        .x(sum2),
+
+        .y_vld(vld_sqrt_a), 
+        .y(sqrt_a)
+    );
+
+    //-------------------------------------------------
+    // FIFO instances
+    //
+
+    flip_flop_fifo_with_counter #(32, 16) fifo_1
+    (
+        .clk(clk),
+        .rst(rst),
+        .push(arg_vld    & ~full_fifo_1 ),
+        .pop (vld_sqrt_c & ~empty_fifo_1),
+
+        .write_data(b),
+        .read_data (out_fifo_1),
+
+        .empty(empty_fifo_1),
+        .full ( full_fifo_1)
+    );
+
+    flip_flop_fifo_with_counter #(32, 33) fifo_2
+    (
+        .clk(clk),
+        .rst(rst),
+        .push(arg_vld    & ~full_fifo_2 ),
+        .pop (vld_sqrt_b & ~empty_fifo_2),
+
+        .write_data(a),
+        .read_data (out_fifo_2),
+
+        .empty(empty_fifo_2),
+        .full ( full_fifo_2)
+    );
+
+    //-------------------------------------------------
+
+    always_ff @(posedge clk or posedge rst)
+    if (rst)
+        res_vld <= 'b0;
+    else
+        res_vld <= vld_sqrt_a;
+    
+    always_ff @(posedge clk)
+    begin
+        vld_sqrt_reg_1 <= vld_sqrt_c;
+        vld_sqrt_reg_2 <= vld_sqrt_b;
+    end
+
+    always_ff @(posedge clk)
+    begin
+        if (vld_sqrt_c)
+            sum1 = sqrt_c + out_fifo_1;
+
+        if (vld_sqrt_b)
+            sum2 = sqrt_b + out_fifo_2;
+
+        if (vld_sqrt_a)
+            res <= sqrt_a;
+    end
+
+    
 
 
 endmodule
